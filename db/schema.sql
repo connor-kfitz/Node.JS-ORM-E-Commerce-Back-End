ea439f764d551df01393562fe16b56dcb52361f4
connor-kfitz <connorkfitzsimmons@gmail.com>
2022-06-25 23:46:28 -0400

    First Commit

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
+-- USE ecommerce_db;
+
+-- CREATE TABLE Category (
+--     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     cateogry_name NOT NULL
+-- );
+
+-- CREATE TABLE Product (
+--     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     product_name NOT NULL,
+--     price DECIMAL NOT NULL 
+-- )
+
